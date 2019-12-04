ff68f72b62fa636c71a0179e8ba83c2b548b3630
ruscat <rus.reg.36@gmail.com>
2019-12-04 19:18:41 +0300

    03-028 структура проекта done

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import  ReactDOM from   'react-dom';
+import  AppHeader from './components/app-header';
+import  SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 ///////////////////////////////////////////////////////////////////////////////////////////////
-const TodoList = ()=>{
-    const items= ['Learn React', 'Build Awesome App']
-   return (
-       <ul>
-           <li>{items[0]}</li>
-           <li>{items[1]}</li>
-       </ul>
-   );
-};
-const AppHeader = ()=>{
-   return (
-       <h1>My Todo List</h1>
-   );
-};
-const SearchPanel = ()=>{
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-    return(
-        <input
-            placeholder={searchText}
-            style={searchStyle}
-        />
-    );
-};
+
+
+
 const App = ()=>{
     return(
         <div>
