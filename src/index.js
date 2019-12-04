e6bed1bb418446e059e59973b925ec76a4f393a0
ruscat <rus.reg.36@gmail.com>
2019-12-04 19:01:50 +0300

    03-028 структура проекта

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import  ReactDOM from   'react-dom';
+///////////////////////////////////////////////////////////////////////////////////////////////
+const TodoList = ()=>{
+    const items= ['Learn React', 'Build Awesome App']
+   return (
+       <ul>
+           <li>{items[0]}</li>
+           <li>{items[1]}</li>
+       </ul>
+   );
+};
+const AppHeader = ()=>{
+   return (
+       <h1>My Todo List</h1>
+   );
+};
+const SearchPanel = ()=>{
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+    return(
+        <input
+            placeholder={searchText}
+            style={searchStyle}
+        />
+    );
+};
+const App = ()=>{
+    return(
+        <div>
+            <span>{(new Date()).toString()}</span>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    );
+};
+let a=1;
+console.log(a);
+///////////////////////////////////////////////////////////////////////////////////////////////////
+ ReactDOM.render(
+     <App/>,
+     document.querySelector('#root')
+ );
