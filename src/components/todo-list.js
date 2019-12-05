8fcc724ecced30629190eef179851751256d9046
ruscat <rus.reg.36@gmail.com>
2019-12-05 09:31:50 +0300

    03-030 массивы как свойства компонентов

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,19 +3,16 @@ import TodoListItem from './todo-list-item';
 
 const TodoList = ({todos})=>{
     const items= ['Learn React', 'Build Awesome App'];
-    const elements = todos.map( ()=>{
-
+    const elements = todos.map( (item)=>{
+        return (<li>
+            <TodoListItem
+                {...item}
+        />
+        </li>)
     });
     return (
         <ul>
-            <li><TodoListItem
-                label={todos[0].label}
-                important={todos[0].important}
-            /></li>
-            <li><TodoListItem
-                label={todos[1].label}
-                important={todos[1].important}
-            /></li>
+            {elements}
         </ul>
     );
 };
