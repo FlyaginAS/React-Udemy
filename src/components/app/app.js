9fba5d31085117123ac09eda73efacbd09c0e47d
ruscat <rus.reg.36@gmail.com>
2019-12-05 11:34:52 +0300

    03-033 структура проекта

---
+++ src/components/app/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import  AppHeader from '../app-header';
+import  SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+const App = ()=>{
+    const todoData = [
+        {label: 'Drink Coffee', important: false, id:1},
+        {label: 'Make Awesome App', important: true, id:2},
+        {label: 'Have a lunch', important: false, id:3},
+    ];
+    return(
+        <div>
+            <span>{(new Date()).toString()}</span>
+            <AppHeader toDo={1} done={3}/>
+            <SearchPanel/>
+            <TodoList todos={todoData}/>
+        </div>
+    );
+};
+export  default App;
