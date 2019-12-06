4d707e2d0ae0507df29432b3f818d826b69961bc
ruscat <rus.reg.36@gmail.com>
2019-12-06 12:42:10 +0300

    03-041 добавление елемента

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,11 +3,13 @@ import  AppHeader from '../app-header';
 import  SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import './app.css';
+import  ItemAddForm from '../item-add-form';
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 export default  class App extends Component{
+    maxId=100;
     state= {
         todoData: [
             {label: 'Drink Coffee', important: false, id:1},
@@ -18,12 +20,28 @@ export default  class App extends Component{
     deleteItem= (id)=>{
        this.setState(({todoData})=>{
             const idx= todoData.findIndex((el)=> el.id===id);
-            const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx+1)];
+            const newArray=todoData.slice();
+            newArray.splice(idx,1);
             return {
                 todoData: newArray,
             }
        });
     };
+    addItem = (text)=>{
+        const newItem= {
+            label: text,
+            important: false,
+            id: this.maxId++,
+        };
+        this.setState(({todoData})=>{
+            let newArr=todoData.slice();
+            newArr.push(newItem);
+            return {
+                todoData: newArr,
+            }
+        });
+    };
+
     render(){
         return (
             <div>
@@ -34,6 +52,7 @@ export default  class App extends Component{
                     todos={this.state.todoData}
                     onDeleted = {this.deleteItem}
                 />
+                <ItemAddForm onItemAdded={this.addItem} />
             </div>
         );
     }
