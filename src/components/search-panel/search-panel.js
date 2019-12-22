523d19fe31ee99f8118c9c4f5d2c358161b45351
ruscat <rus.reg.36@gmail.com>
2019-12-22 13:25:41 +0300

    05-048 реализуем фильтры

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -3,16 +3,29 @@ import React from 'react';
 import './search-panel.css';
 import ItemStatusFilter from '../item-status-filter';
 
-const SearchPanel = () => {
-  return (
-      <React.Fragment>
-          <input type="text"
-                 className="form-control search-input"
-                 placeholder="type to search" />
-          <ItemStatusFilter/>
-      </React.Fragment>
+class SearchPanel extends React.Component {
+    state={
+       term: ''
+    };
+    onSearchChange = (e)=> {
+       const term = e.target.value;
+       this.setState({term});
+       this.props.onSearchChange(term);
+    };
+    render(){
+        return (
+            <React.Fragment>
+                <input type="text"
+                       className="form-control search-input"
+                       placeholder="type to search"
+                       value={this.state.term}
+                       onChange={this.onSearchChange}
+                />
+                {/*<ItemStatusFilter/>*/}
+            </React.Fragment>
 
-  );
-};
+        );
+    }
+}
 
 export default SearchPanel;
